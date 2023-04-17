f9cc1f564521d791a9581bbb1564830a11f25e8c
HovHovsepyan09 <128827283+HovHovsepyan09@users.noreply.github.com>
2023-04-17 15:35:56 +0400

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,16 @@
+# import math
+
+# class Covid_19:
+#     def __init__(self,temp: float) -> None:
+#         self.body_temperetaur = math.pi*temp
+#     def covid_or_not(self):
+#         '''
+#         If you duplicate your body temperature by
+#         Celsus with the number of pi(math) and round up to the nearest whole number and if it is
+#         between the intervals of 120 to 128 so you have coronavirus otherwise no.
+#         '''
+
+#         if 120 <= math.ceil(self.body_temperetaur) < 128:
+#             print("You Have coronavirus")
+#         else:
+#             print("Everything is okay")
